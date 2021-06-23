5ed255b38358a1ed5a55e0f89030364273a46e50
Juan Pablo Garcia <45505790+JPGarCar@users.noreply.github.com>
2021-06-23 12:07:36 -0700

    Small icon fix, and table style improved in first column.

--- render.php
+++ render.php
@@ -257,12 +257,16 @@ if ($_GET['taxon-search'] ?? null) {
                         <?php foreach ($tableData->getTableRows(DATABASE) as $tableRow): ?>
                             <tr class="conditional-hover-background">
                                 <!-- row header with link to go to specimen page -->
-                                <th scope="row">
+                                <th scope="row" class="text-nowrap">
                                     <a href="details.php?Database=<?=$tableRow->getUrl()?>" role="button" class="conditional-text-color">
-                                        <?php if ($tableRow->isHasImage()): ?>
-                                            <span class="oi oi-image"></span>
-                                        <?php endif; ?>
                                         <b><?= $tableRow->getId() ?></b>
+
+                                        <?php if ($tableRow->isHasImage()): ?>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-image " viewBox="0 0 16 16">
+                                                <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                                                <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54A.505.505 0 0 1 1 12.5v-9a.5.5 0 0 1 .5-.5h13z"/>
+                                            </svg>
+                                        <?php endif; ?>
                                     </a>
                                 </th>
 
